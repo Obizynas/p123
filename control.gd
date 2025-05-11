768e328e0a7b01923be8731301a94b9ffe9ea6c6
huuughs <susukistas0307@gmail.com>
2025-05-11 19:01:20 +0300

    launguage

--- control.gd
+++ control.gd
@@ -1,1 +1,12 @@
 extends Control
+
+func _ready():
+	$SmallPanel.visible = false  # гарантуємо, що панель прихована з початку
+	$Button.pressed.connect(_on_main_button_pressed)
+	$SmallPanel/CloseButton.pressed.connect(_on_close_button_pressed)
+
+func _on_main_button_pressed():
+	$SmallPanel.visible = true  # показати панель при натисканні кнопки
+
+func _on_close_button_pressed():
+	$SmallPanel.visible = false  # ховати панель при натисканні кнопки "Закрити"
